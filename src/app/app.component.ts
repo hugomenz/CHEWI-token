0e7217a7f8b177f045345f7a7515bbd7559bc7da
Emper5 <tni.gabriel@gmail.com>
2021-12-08 14:55:29 +0000

    Se añade contenido a whitepaper y se corrigen cosa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { faDiscord, faFacebook, faTelegram, faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
@@ -6,9 +7,19 @@ import { faDiscord, faFacebook, faTelegram, faTwitter} from '@fortawesome/free-b
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chewi';
-  showFiller = false;
+export class AppComponent implements OnInit {
+  constructor(private meta: Meta,private title: Title) {
+    this.meta.addTags([
+      {name: 'getChewis', content: 'Join the resistance'},
+      {name: 'whitepaper', content: 'Whitepaper'}
+    ]);
+    this.setTitle('Chewi Token');
+  }
+  public setTitle( newTitle: string) {
+    this.title.setTitle( newTitle );
+    }
+  ngOnInit() {
+  }
 
   faFacebook = faFacebook;
   faTelegram = faTelegram;
